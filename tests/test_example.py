496d31dd67d3bca09d45587c67b65859ec612d4f
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-08-29 19:42:24 -0300

    Criação do teste da documentação do playwright e inicio do teste do youtube

---
+++ tests/test_example.py
@@ -0,0 +1,15 @@
+import re 
+from playwright.sync_api import Page, expect #importando o playwright api e usando-o como "re", por algum motivo não pode na mesma linha
+
+def test_has_title(page: Page):
+    # Navega para a página do Playwright (ou qualquer outra url que eu colocar)
+    page.goto("https://playwright.dev/")
+
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def test_get_started_link(page: Page):
+    page.goto("https://playwright.dev/")
+
+    page.get_by_role("link", name="Get started").click()
+
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
---
+++ tests/testesAutomacoes/test_youtube.py
@@ -0,0 +1,3 @@
+#importando o playwright api e usando-o como "re", por algum motivo não pode na mesma linha
+import re 
+from playwright.sync_api import Page, expect
