4134e62dd00f4dc9165f29bbabbb7ea6024dea70
FelipeSardinhaMiguel <felipesardinham@gmail.com>
2026-08-29 23:30:55 -0300

    abro o link e confirmo que a url contem 'youtube' no test_youtube

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,6 +5,7 @@ def test_has_title(page: Page):
     # Navega para a página do Playwright (ou qualquer outra url que eu colocar)
     page.goto("https://playwright.dev/")
 
+    #Uma espécie de if que serve para verificar se o titulo da página (to_have_title) realmente contem a palavra que eu escrever ("Playwright"), assim da pra confirmar que o teste funcionou.
     expect(page).to_have_title(re.compile("Playwright"))
 
 def test_get_started_link(page: Page):
--- tests/testesAutomacoes/test_youtube.py
+++ tests/testesAutomacoes/test_youtube.py
@@ -1,3 +1,10 @@
 #importando o playwright api e usando-o como "re", por algum motivo não pode na mesma linha
-import re 
-from playwright.sync_api import Page, expect
+import re #isso é do python.
+from playwright.sync_api import Page, expect #isso é do playwright.
+
+def test_youtube(page: Page):
+    page.goto("https://www.youtube.com/")
+
+    expect(page).to_have_title(re.compile("youtube"))
+
+
